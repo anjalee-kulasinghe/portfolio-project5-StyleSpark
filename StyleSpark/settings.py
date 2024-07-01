eaba702bcfa86d5fe851edb96c6f352f7924162d
Anjalee Kulasinghe <anjaleek@gmail.com>
2024-07-01 19:45:14 +0000

    Add the context file to the cart

--- StyleSpark/settings.py
+++ StyleSpark/settings.py
@@ -74,6 +74,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'cart.contexts.cart_contents',
             ],
         },
     },
@@ -158,3 +159,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STANDARD_DELIVERY_PERCENTAGE = 10
