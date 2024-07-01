ed6e29af4a8617861a23429c6d87fc9027bbdde5
Anjalee Kulasinghe <anjaleek@gmail.com>
2024-07-01 21:15:06 +0000

    Modify the context file for the processor

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,11 +1,25 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 def cart_contents(request):
 
     cart_items = []
     total = 0
     product_count = 0
+    cart = request.session.get('cart', {})
+
+    for item_id, quantity in cart.items():
+        product = get_object_or_404(Product, id=item_id)
+        total += quantity * product.price
+        product_count += quantity
+        cart_items.append({
+        	'item_id': item_id,
+        	'quantity': quantity,
+        	'product': product,
+    	})
+
 
     delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
     grand_total = delivery + total
