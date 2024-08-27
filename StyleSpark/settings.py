1d6137e7138c26e9955e1734878648e15c4d3367
Anjalee Kulasinghe <anjaleek@gmail.com>
2024-08-27 09:24:41 +0000

    remove the secret key and the debug

--- StyleSpark/settings.py
+++ StyleSpark/settings.py
@@ -23,16 +23,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$^pc4vz(bs@axk7wlv5vtrvq*%7efaz@sufz5dheb%8+3_^zj9'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = [
     '.herokuapp.com',
     'localhost',
     '127.0.0.1',
-    '8000-anjaleekula-portfoliopr-5x9c2lo43p2.ws.codeinstitute-ide.net'
+    '8000-anjaleekula-portfoliopr-5x9c2lo43p2.ws.codeinstitute-ide.net',
+    'project5-stylespark-367466e38579.herokuapp.com'
 ]
 
 
