4ab04e4040167c84f12e0313e6bb83c406a489b2
Anjalee Kulasinghe <anjaleek@gmail.com>
2024-08-27 09:12:28 +0000

    Update ALLOWED_HOSTS to include Heroku domain

--- StyleSpark/settings.py
+++ StyleSpark/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-$^pc4vz(bs@axk7wlv5vtrvq*%7efaz@sufz5dheb%8+3_^zj9
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    'project5-stylespark.herokuapp.com',
+    '.herokuapp.com',
     'localhost',
     '127.0.0.1',
     '8000-anjaleekula-portfoliopr-5x9c2lo43p2.ws.codeinstitute-ide.net'
